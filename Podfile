1caca411d05086601a104613a070d6b848d5a3d4
Shashikant86 <shashikant.jagtap@icloud.com>
2017-12-20 22:50:21 +0000

    Fastlane Swift Demo

---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '11.0'
+
+target 'FastlaneSwiftDemo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  target 'FastlaneSwiftDemoTests' do
+    inherit! :search_paths
+    pod "Hippolyte"
+  end
+
+  target 'FastlaneSwiftDemoUITests' do
+    inherit! :search_paths
+    pod "XCFit"
+  end
+
+end
---
+++ fastlane/Appfile.swift
@@ -0,0 +1,8 @@
+var appIdentifier: String { return "x" } // The bundle identifier of your app
+var appleID: String { return "x" } // Your Apple email address
+
+var teamID: String { return "[[DEV_PORTAL_TEAM_ID]]" } // Developer Portal Team ID
+var itcTeam: String? { return nil } // iTunes Connect Team ID (may be nil if no team)
+
+// you can even provide different app identifiers, Apple IDs and team names per lane:
+// More information: https://docs.fastlane.tools/advanced/#appfile
